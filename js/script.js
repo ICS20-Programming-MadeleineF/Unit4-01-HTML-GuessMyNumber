d31516004b50d07ff4af9f3d4da0da52120ebb38
Madeleine Forgeron <99274168+MadeleineForgeron@users.noreply.github.com>
2022-04-26 14:47:41 +0000

    do css

--- js/script.js
+++ js/script.js
@@ -9,3 +9,22 @@
  * This function calculates random number.
  */
 function makeRandomNumber() {
+   	// initialize variables
+	let number = ""
+	
+	// get user input
+	let guess = parseInt(document.getElementById('guess').value)
+
+
+  	// if hour is before noon, display "Good morning"
+	if (guess == Math.floor(Math.random() * 6) + 1){
+		number = "CORRECT!!!"
+	} 
+	// otherwise, if hour is before 6pm, display "Good afternoon"	
+	else {
+		number = "INCORRECT"
+	}
+
+  // display the results
+  document.getElementById('number').innerHTML = number
+}
