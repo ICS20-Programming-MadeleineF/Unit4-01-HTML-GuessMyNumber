413aa6848e54c0fc30f55742058874cf7d8799bc
Madeleine Forgeron <99274168+MadeleineForgeron@users.noreply.github.com>
2022-04-26 14:39:48 +0000

    do jsa

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Madeleine Forgeron All rights reserved
+//
+// Created by: Madeleine Forgeron
+// Created on: April 2022
+// This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates random number.
+ */
+function makeRandomNumber() {
